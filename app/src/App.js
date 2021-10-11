e78f68485cbbd20ebdff090a8d93bb6b2944a1c4
Ksahitya <ksahitya2@gmail.com>
2021-10-11 15:32:21 -0400

    sahitya change

--- app/src/App.js
+++ app/src/App.js
@@ -15,11 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-<<<<<<< HEAD
           Learn React with me sahitya
-=======
-          Learn React with yaswanth
->>>>>>> origin/main
         </a>
       </header>
     </div>
