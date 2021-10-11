089411b454161712df4885117d09e38890c557f3
Ksahitya <ksahitya2@gmail.com>
2021-10-11 13:01:45 -0400

    i love it

--- app/src/App.js
+++ app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React .. I Love it 
         </a>
       </header>
     </div>
