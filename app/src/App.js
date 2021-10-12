edf05fa387db24123497f42d201699247ee31a39
Ksahitya <ksahitya2@gmail.com>
2021-10-12 15:38:08 -0400

    well

--- app/src/App.js
+++ app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          keep calm and learn react
+          learn react well
         </a>
       </header>
     </div>
